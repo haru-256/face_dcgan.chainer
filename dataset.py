6ab98bb389cb135e92e0ec8be2a3ff0b83711870
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-04-30 22:34:29 +0900

    hoge

--- dataset.py
+++ dataset.py
@@ -6,4 +6,8 @@ import pathlib
 class FaceData(chainer.dataset.DatasetMixin):
 
     def __init__(self):
-        path = pathlib.Path("")
+        data_path = pathlib.Path("data")
+        abs_data_path=data_path.resolve()
+        print("data path:", abs_data_path)
+        for img_path in data_path.glob(*/*.jpg):
+            img = cv2.imread()
