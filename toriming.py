75ad69ec6a850358e6e4f612464fa6302dea5fd3
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-04-30 18:13:37 +0900

    preprocessing

--- toriming.py
+++ toriming.py
@@ -1,23 +1,25 @@
 import cv2
 import pathlib
-import matplotlib.pyplot as plt
 
-data_path = pathlib.Path("Yukio_Hatoyama_0001.jpg")
+
+data_path = pathlib.Path("../data/lfw/")
 abs_data_path = data_path.resolve()
-save_path = abs_data_path.parent / "cropped_data/"
+save_path = pathlib.Path(".") / "cropped_data_128"
+abs_save_path = save_path.resolve()
 
-if not save_path.exists():
-    save_path.mkdir()
+if not abs_save_path.exists():
+    abs_save_path.mkdir()
 
-print("save_path:", save_path)
+print("save dir:", abs_save_path)
 
-img = cv2.imread(str(data_path))
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
 # center crop 150*150
-dx, dy = 150, 150
-cropped_img = img[125 - dx // 2:125 + dx // 2, 125 - dy // 2:125 + dy // 2]
-plt.imshow(cropped_img)
-plt.show()
-
-# save
-cv2.imwrite(str(save_path /data_path), cropped_img)
+dx, dy = 128, 128
+for i, img_path in enumerate(abs_data_path.glob("*/*.jpg")):
+    img = cv2.imread(str(img_path))
+    cropped_img = img[125 - dx // 2:125 + dx // 2, 125 - dy // 2:125 + dy // 2]
+    # save
+    tmp = pathlib.Path(str(abs_save_path / img_path.parts[-2]))
+    if not tmp.exists():
+        tmp.mkdir()
+    cv2.imwrite(str(abs_save_path / "/".join(img_path.parts[-2:])), cropped_img)
