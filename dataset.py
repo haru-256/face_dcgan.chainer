e1084c0673d8852158ba24c22eb6e755341fc0f7
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-04-30 23:38:51 +0900

    dataset

--- dataset.py
+++ dataset.py
@@ -1,13 +1,32 @@
 import chainer
 import cv2
 import pathlib
+"""
+ChainerのDatasetは
+初期化 __init__
+内部に持っているデータの数を返却する __len__(self)
+このメソッドは整数でデータ数を返却する
+i番目のデータを取得する get_example(self, i)
+を実装する
+このメソッドは、return image_array, labelの様なことをする
+"""
 
 
 class FaceData(chainer.dataset.DatasetMixin):
-
+    """
+        cropped_data_128ディレクトリからデータを読み込む
+    """
     def __init__(self):
-        data_path = pathlib.Path("data")
-        abs_data_path=data_path.resolve()
+        data_path = pathlib.Path("cropped_data_128")
+        abs_data_path = data_path.resolve()
         print("data path:", abs_data_path)
-        for img_path in data_path.glob(*/*.jpg):
-            img = cv2.imread()
+        # Reading data
+        self.data = [
+            cv2.imread(img_path) for img_path in abs_data_path.glob("*/*.jpg")
+        ]
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_example(self, i):
+        return self.data[i]
