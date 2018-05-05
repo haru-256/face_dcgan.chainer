259b485ce7d0d5819a6b7eb243cff90c1c106b65
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-05-06 00:00:24 +0900

    image dataset

---
+++ image_dataset.py
@@ -0,0 +1,17 @@
+# %%
+from chainer.datasets import ImageDataset
+import pathlib
+
+data_dir = pathlib.Path("cropped_data/")
+data_path = []
+for path in data_dir.glob("*.jpg"):
+    data_path.append(path)
+print(data_path)
+data = ImageDataset(paths=data_path)
+print(data)
+image = data.get_example(0)
+print(data.get_example(0).shape)
+
+# %%
+import matplotlib.pyplot as plt
+plt.imshow(image.transpose(1, 2, 0).astype("int"))
