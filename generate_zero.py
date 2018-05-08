78d1ef81614f60e4c717da792510e3bbd8e64ba0
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-05-08 14:58:09 +0900

    Confirmed that A occurs

--- generate_zero.py
+++ generate_zero.py
@@ -6,16 +6,17 @@ from visualize import combine_images
 import pathlib
 import matplotlib.pyplot as plt
 
-path = pathlib.Path("result")
+path = pathlib.Path("result3_a/gen_iter_31015.npz")
 abs_path = path.resolve()
 
 gen = Generator()  # prepare model
-serializers.load_npz(path/"gen_iter_9305.npz", gen) # load pretraining model
+serializers.load_npz(path, gen)  # load pretraining model
 # serializers.load_npz(path/"gen_iter_1406250.npz", gen) # load pretraining model
 xp = gen.xp  # get numpy or cupy
 
 xp.random.seed(144)
-z = Variable(xp.random.uniform(-1, 1, (10*10, 100)).astype("f"))  # make noize whose elements are all
+z = Variable(xp.random.uniform(
+    -1, 1, (10 * 10, 100)).astype("f"))  # make noize whose elements are all
 
 with chainer.using_config('train', False):
     x = gen(z)
@@ -27,5 +28,5 @@ x = combine_images(x)
 plt.figure(figsize=(10, 10))
 plt.imshow(x)
 plt.axis("off")
-plt.savefig("input_zeros.png")
+plt.savefig("hoge.png")
 plt.show()
